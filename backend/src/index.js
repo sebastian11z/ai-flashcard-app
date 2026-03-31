304a2dce5960388666389ba80b389dd22c5c561a
Sebastian Garcia <sebastiangarciaa27@gmail.com>
2026-03-31 13:59:33 +0800

    backend adjustments

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    origin: ['http://localhost:5173', 'https://ai-flashcard-app-ebon.vercel.app/login'],
     credentials: false,
   })
 );
